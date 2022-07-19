f041f8dc981cdb587aca88bc3f5d84f5677c7045
RussellShire <russell.shire@gmail.com>
2022-07-19 15:34:18 +0100

    implementing modules

--- backstory.js
+++ backstory.js
@@ -1,12 +1,13 @@
-const bsOpenPt1 = ['Despite being', 'After being', 'Because they were', 'Growing up']
-const bsOpenPt2 = ['raised by', 'abandoned by', 'adopted by', 'trained by', 'schooled by', 'attacked by']
-const bsAdjectives = ['incredible', 'terrifying', 'helpful', 'dangerous', 'aggresive', 'fantastic']
-const bsProfession = ['clown', 'chef', 'solider', 'parent', 'criminal', 'teenager', 'farmer', 'acrobat', 'servant', 'wild animal', 'priest', 'teacher', 'grave digger']
-const bsPastTenseAdj = ['struggled', 'fought bravely', 'flourished', 'failed', 'tried hard', 'learned']
-const bsProfessionJoin = ['to become a', 'to train as a', 'to avoid becoming a', 'to study as a']
+const bsOpenPt1 = ['Despite being', 'After being', 'Because they were', 'Growing up', 'Coming of age', 'Entering their youth']
+const bsOpenPt2 = ['raised by', 'attacked by', 'robbed by', 'abandoned by', 'adopted by', 'trained by', 'schooled by', 'attacked by', 'influenced by']
+const bsAdjectives = ['incredible', 'terrifying','unhelpful', 'helpful', 'beautiful', 'hidious', 'brave', 'wild', 'interesting', 'flamboyant', 'nervous', 'intense', 'dangerous', 'aggresive', 'fantastic']
+const bsProfession = ['clown', 'drunk', 'labourer', 'sailor', 'lawyer', 'guard', 'chef', 'idiot', 'shop keeper', 'solider', 'parent', 'criminal', 'teenager', 'farmer', 'acrobat', 'servant', 'animal', 'priest', 'teacher', 'grave digger']
+const bsPastTenseAdj = ['struggled', 'fought bravely', 'failed', 'tried hard', 'learned', 'overcame adversity', 'cheated their way']
+const bsProfessionJoin = ['to become a', 'to graduate as a', 'to avoid becoming a', 'to study as a']
 
-const randArray = array => array[Math.floor(Math.random() * (array.length-1))]
+const randArray = array => array[Math.floor(Math.random() * array.length)]
 
-const backstory = () => console.log(`${randArray(bsOpenPt1)} ${randArray(bsOpenPt2)} ${randArray(bsAdjectives)} ${randArray(bsProfession)}s they ${randArray(bsPastTenseAdj)} ${randArray(bsProfessionJoin)} ${randArray(bsProfession)}, which led them here`)
+const backstory = () => 
+    `${randArray(bsOpenPt1)} ${randArray(bsOpenPt2)} ${randArray(bsAdjectives)} ${randArray(bsProfession)}s they ${randArray(bsPastTenseAdj)} ${randArray(bsProfessionJoin)} ${randArray(bsAdjectives)} ${randArray(bsProfession)}, which led them here`
  
-backstory()
+export {backstory}
--- main.js
+++ main.js
@@ -1,32 +1,22 @@
-//Arrays of statements to be joined to make a back story
-const bsOpenPt1 = ['Despite being', 'After being', 'Because they were', 'Growing up']
-const bsOpenPt2 = ['raised by', 'abandoned by', 'adopted by', 'trained by', 'schooled by', 'attacked by']
-const bsAdjectives = ['incredible', 'terrifying', 'helpful', 'beautiful', 'hidious', 'brave', 'wild', 'intense', 'dangerous', 'aggresive', 'fantastic']
-const bsProfession = ['clown', 'chef', 'idiot', 'shop keeper', 'solider', 'parent', 'criminal', 'teenager', 'farmer', 'acrobat', 'servant', 'wild animal', 'priest', 'teacher', 'grave digger']
-const bsPastTenseAdj = ['struggled', 'fought bravely', 'flourished', 'failed', 'tried hard', 'learned']
-const bsProfessionJoin = ['to become a', 'training as a', 'to avoid becoming a', 'to study as a']
+//Bringing in a complicated function from another module
+import { backstory } from './backstory.js';
 
-//A function to return a random array
-const randArray = array => array[Math.floor(Math.random() * array.length)]
-
-//A function that randomly stitches together pieces of text to create a backstory
-const backstory = () => `${randArray(bsOpenPt1)} ${randArray(bsOpenPt2)} ${randArray(bsAdjectives)} ${randArray(bsProfession)}s they ${randArray(bsPastTenseAdj)} ${randArray(bsProfessionJoin)} ${randArray(bsProfession)}, which led them here`
- 
 //A class for making monsters
 class Monsters {
     constructor (type, size, alignment) {
         this.type = type;
         this.size = size;
         this.alignment = alignment;
-        this.backstory = backstory();
+        this.backstory = backstory(); //This function is in another module
         }
     get topLine() {
         return `A ${this.size} ${this.type}, they are ${this.alignment}.`;
         }
 }
-            
+          
 //creating some monsters
 const goblin = new Monsters('humaniod', 'small', 'neutral evil');
 const orc = new Monsters('humaniod', 'medium', 'chaotic evil');
 
-console.log(`${goblin.topLine} ${goblin.backstory}`)
+console.log(`${goblin.topLine} 
+${goblin.backstory}`)
