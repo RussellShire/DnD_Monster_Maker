9803464fbe580f3d1f6296b32831a1d3ecda7357
RussellShire <russell.shire@gmail.com>
2022-07-20 13:07:51 +0100

    refactored to use parent/child classes. Added hitpoints

--- main.js
+++ main.js
@@ -8,20 +8,58 @@ class Monsters {
         this.type = type;
         this.size = size;
         this.alignment = alignment;
-        this.backstory = backstory(); //This function is in another module
         }
     get topLine() {
-        return `${this.race}s: Are ${this.size} ${this.type}s, they are ${this.alignment}.`;
+        return `${this.race}s are ${this.size} ${this.type}s, they are ${this.alignment}.`;
         }
+    get bio() {
+        return `${this.name} is a ${this.race}, ${this.topLine} 
+    ${this.backstory}
+    ${this.name} has ${this.hitpoints} hitpoints`
+    }
 }
-          
-//creating some monsters
-const goblin = new Monsters('Goblin', 'humaniod', 'small', 'neutral evil');
-const orc = new Monsters('Orc', 'humaniod', 'medium', 'chaotic evil');
 
-console.log(`${goblin.topLine} 
-${goblin.backstory}`)
+let diceRoll = sides => Math.floor(Math.random()*sides+1);
 
-console.log(`${orc.topLine} 
-${orc.backstory}`)
+let hitpoints = (dice, sides, modify) => {
+    let dieCounter = 0
+    let hp = 0
+    while (dieCounter < dice){
+        dieCounter++
+        hp += diceRoll(sides);
+    }
+    return hp += modify
+}
 
+//Child Class of Monsters for a monster type
+class Goblin extends Monsters {
+    constructor(name, race, type, size, alignment){
+        super(race, type, size, alignment)
+            this.name = name;
+            this.race = 'goblin';
+            this.type = 'humaniod';
+            this.size = 'small';
+            this.alignment = 'neutral evil'
+            this.backstory = backstory(); //This function is in another module, calling here means each Goblin gets a backstory
+            this.hitpoints = hitpoints(2,6,0) //function takes dice 2 Sides D6 and modifier 0 to roll hitpoints
+    }
+}
+//Another monster type
+class Orc extends Monsters {
+    constructor(name, race, type, size, alignment){
+        super(race, type, size, alignment)
+            this.name = name;
+            this.race = 'orc';
+            this.type = 'humaniod';
+            this.size = 'medium';
+            this.alignment = 'chaotic evil'
+            this.backstory = backstory(); //This function is in another module
+            this.hitpoints = hitpoints(2,8,6) //Rolls 2D8+6
+    }
+}
+
+//creating some individual monsters
+const steve = new Goblin('Steve')
+const geoff = new Orc('Geoff');
+
+console.log(geoff.bio)
